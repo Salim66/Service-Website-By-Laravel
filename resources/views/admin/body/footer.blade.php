5b86e1ec76e54ed0bca96b134464ac55ad7b10bb
Salim66 <salimhasanriad8@gmail.com>
2022-06-15 15:38:54 +0600

    Refresh admin template

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -5,9 +5,9 @@
 			<a class="nav-link" href="javascript:void(0)">FAQ</a>
 		  </li>
 		  <li class="nav-item">
-			<a class="nav-link" href="#">Purchase Now</a>
+			<a class="nav-link" href="www.techdynobd.com">www.techdynobd.com</a>
 		  </li>
 		</ul>
     </div>
-	  &copy; 2020 <a href="#">Psd to Html Expert</a>. All Rights Reserved.
+	  &copy; 2022 <a href="www.techdynobd.com">Techdyno BD LTD</a>. All Rights Reserved.
 </footer>
