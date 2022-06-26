d0a7834afbe9bf38329d78167bb6846735bc40df
Salim66 <salimhasanriad8@gmail.com>
2022-06-26 15:27:04 +0600

    Hero Slider Frontend Done

--- resources/views/frontend/body/hero.blade.php
+++ resources/views/frontend/body/hero.blade.php
@@ -1,58 +1,24 @@
+   @php
+       $all_data = \App\Models\Slider::latest()->get();
+   @endphp
    <!-- Bnner Section -->
    <section class="banner-section">
     <div class="banner-carousel owl-carousel owl-theme">
 
         <!-- Slide Item -->
-        <div class="slide-item" style="background-image: url(frontend/images/main-slider/1.jpg);">
+        @foreach($all_data as $data)
+        <div class="slide-item" style="background-image: url({{ URL::to($data->slider_img) }});">
             <div class="auto-container clearfix">
                 <!-- Content Box -->
                 <div class="content-box">
                     <div class="inner">
-                        <h2>Bright Solutions.</h2>
-                        <div class="text">Independent Personal & business advice based on established <br> research methods & knowledge experts.</div>
-                        <div class="link-box">
-                            <a href="about.html" class="theme-btn btn-style-two"><i>More About Us</i> <span class="arrow flaticon-next-8"></span></a>
-                            <a href="about.html" class="theme-btn learn-more">Industries We Covered <i class="flaticon-next-8"></i></a>
-                        </div>
+                        <h2>{{ $data->slider_title }}</h2>
+                        <div class="text">{{ $data->slider_descp }}</div>
                     </div>
                 </div>
             </div>
         </div>
-
-        <!-- Slide Item -->
-        <div class="slide-item" style="background-image: url(frontend/images/main-slider/2.jpg);">
-            <div class="auto-container clearfix">
-                <!-- Content Box -->
-                <div class="content-box right-box">
-                    <div class="inner">
-                        <h2>Intelligent Choice.</h2>
-                        <div class="text">Independent Personal & business advice based on established <br> research methods & knowledge experts.</div>
-                        <div class="link-box">
-                            <a href="about.html" class="theme-btn btn-style-two"><i>More About Us</i> <span class="arrow flaticon-next-8"></span></a>
-                            <a href="about.html" class="theme-btn learn-more">Industries We Covered <i class="flaticon-next-8"></i></a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
-        <!-- Slide Item -->
-        <div class="slide-item" style="background-image: url(frontend/images/main-slider/3.jpg);">
-            <div class="auto-container clearfix">
-                <!-- Content Box -->
-                <div class="content-box">
-                    <div class="inner">
-                        <h2>Get Real Results.</h2>
-                        <div class="text">Independent Personal & business advice based on established <br> research methods & knowledge experts.</div>
-                        <div class="link-box">
-                            <a href="about.html" class="theme-btn btn-style-two"><i>More About Us</i> <span class="arrow flaticon-next-8"></span></a>
-                            <a href="about.html" class="theme-btn learn-more">Industries We Covered <i class="flaticon-next-8"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        @endforeach
 
     </div>
 </section>
