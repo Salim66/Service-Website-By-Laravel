7a2e6adec97d5720ffae002f151847c207289ff7
Salim66 <salimhasanriad8@gmail.com>
2022-06-27 19:40:04 +0600

    Some functionality change

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -19,39 +19,54 @@
                             </button>
                         </div>
 
-                        @php
-                            $categories = \App\Models\Category::limit(3)->get();
-                        @endphp
+                        
 
                         <div class="collapse navbar-collapse clearfix" id="navbarSupportedContent">
                             <ul class="navigation clearfix">
-                                @foreach($categories as $category)
-                                <li class="dropdown"><a href="{{ url('/category/services/'.$category->id.'/'.$category->category_slug) }}">{{ $category->category_name }}</a>
+
+                                <li><a href="{{ route('about.us') }}">About Us</a></li>
+
+                                @php
+                                    $categories = \App\Models\Category::limit(3)->get();
+                                @endphp
+
+                                <li class="dropdown"><a href="javascipt:void(0)">Services</a>
                                     <ul>
-                                        @php
-                                                $subcategories = App\Models\SubCategory::where('category_id', $category->id)->orderBy('subcategory_name', 'ASC')->get();
-                                        @endphp
+                                        @foreach($categories as $category)
+                                            <li class="dropdown"><a href="{{ url('/category/services/'.$category->id.'/'.$category->category_slug) }}">{{ $category->category_name }}</a>
 
-                                        @foreach($subcategories as $sub)
-                                        <li class="dropdown"><a href="{{ url('/subcategory/services/'.$sub->id.'/'.$sub->subcategory_slug) }}">{{ $sub->subcategory_name }}</a>
+                                                <ul>
+                                                    @php
+                                                            $subcategories = App\Models\SubCategory::where('category_id', $category->id)->orderBy('subcategory_name', 'ASC')->get();
+                                                    @endphp
 
-                                            <ul>
-                                                @php
-                                                    $subsubcategories = App\Models\SubSubCategory::where('subcategory_id', $sub->id)->orderBy('subsubcategory_name', 'ASC')->get();
-                                                @endphp
+                                                    @foreach($subcategories as $sub)
+                                                        <li class="dropdown"><a href="{{ url('/subcategory/services/'.$sub->id.'/'.$sub->subcategory_slug) }}">{{ $sub->subcategory_name }}</a>
 
-                                                @foreach($subsubcategories as $subsub)
-                                                <li><a href="{{ url('/subsubcategory/services/'.$subsub->id.'/'.$subsub->subsubcategory_slug) }}">{{ $subsub->subsubcategory_name }}</a></li>
-                                                @endforeach
-                                            </ul>
-                                        </li>
+                                                            <ul>
+                                                                @php
+                                                                    $subsubcategories = App\Models\SubSubCategory::where('subcategory_id', $sub->id)->orderBy('subsubcategory_name', 'ASC')->get();
+                                                                @endphp
+                
+                                                                @foreach($subsubcategories as $subsub)
+                                                                <li><a class="dropdown_last" href="{{ url('/subsubcategory/services/'.$subsub->id.'/'.$subsub->subsubcategory_slug) }}">{{ $subsub->subsubcategory_name }}</a></li>
+                                                                @endforeach
+                                                            </ul>
+
+                                                        </li>
+                                                    @endforeach
+                                                </ul>
+
+                                            </li>
                                         @endforeach
+
+                                        
                                     </ul>
                                 </li>
-                                @endforeach
+
                                
                                 <li><a href="{{ route('blog') }}">Blog</a></li>
-                                <li><a href="{{ route('contact.us') }}">Contact</a></li>
+                                <li><a href="{{ route('contact.us') }}">Contact Us</a></li>
                             </ul>
                         </div>
                     </nav>
